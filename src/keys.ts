bbc307102252627bd21a6a0c3f67545c7ee043f5
Ricardo Hernandez Limon <richi8297@gmail.com>
2022-09-10 22:29:45 -0500

    Keys cambiadas otravez

--- src/keys.ts
+++ src/keys.ts
@@ -2,10 +2,10 @@
 export default{
     database:{
         //Cambiar por la ip dada por el host
-        host: 'containers-us-west-41.railway.app',
+        host: 'containers-us-west-69.railway.app',
         user: 'root',
-        password: '1L0r7GnylIaxPYf3CAzz723',
+        password: 'vyfm3BW5rS2pzZnJya4K',
         database: 'railway',
-        port: '6619'
+        port: '5705'
     }
 }
