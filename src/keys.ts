26422b0dba2d3f557db0590c78f246cf7f020a5a
Ricardo Hernandez Limon <richi8297@gmail.com>
2022-09-11 13:07:46 -0500

    6.2 keys

--- src/keys.ts
+++ src/keys.ts
@@ -2,9 +2,10 @@
 export default{
     database:{
         //Cambiar por la ip dada por el host
+        port: '7019',
         host: 'containers-us-west-72.railway.app',
         user: 'root',
         password: 'Flx93iol9tZ9vl225XiM',
-        database: 'railway'  
+        database: 'railway' 
     }
 }
